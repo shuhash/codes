3b9607bf5bf91d319b1a815672214320a5af7986
shuhash <60608897+shuhash@users.noreply.github.com>
2021-04-24 09:29:44 +0530

    Add files via upload

---
+++ port scanning using socket.py
@@ -0,0 +1,32 @@
+import socket
+import subprocess
+import sys
+
+subprocess.call('clear', shell=True)
+
+remoteServer    = input("Enter a remote host to scan: ")
+remoteServerIP  = socket.gethostbyname(remoteServer)
+
+print("-" * 60)
+print("Please wait, scanning remote host", remoteServerIP)
+print("-" * 60)
+
+try:
+    for port in range(1,1025):  
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print("Port {}: 	 Open".format(port))
+        sock.close()
+
+except KeyboardInterrupt:
+    print("You pressed Ctrl+C")
+    sys.exit()
+
+except socket.gaierror:
+    print('Hostname could not be resolved. Exiting')
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server")
+    sys.exit()
