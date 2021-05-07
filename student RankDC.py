c8d8b9a39d8f049f4a36d639adab9b52940eb9d4
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-05-07 09:12:21 +0530

    Create student RankDC.py

---
+++ student RankDC.py
@@ -0,0 +1,4 @@
+n=int(input())   ###
+ l=l(l(map(int,input().split())) ###
+ x=int(input()) ###
+(lambda n,l,x:print(sorted(set(l))[::-1].index(l[x-1])+1))(int(input()),list(map(int,input().split())),int(input()))
