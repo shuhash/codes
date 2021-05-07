593ab2a5265382cdf94ee68b8f3006feacb77aeb
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-05-07 09:14:38 +0530

    Update function merge evenDC.c

--- function merge evenDC.c
+++ function merge evenDC.c
@@ -16,3 +16,34 @@ int* mergeEven(int M, int *arr1, int N, int *arr2)
     return arr;
 
 }
+int main()
+{
+    int M, N;
+    scanf("%d %d", &M, &N);
+    int arr1[M], arr2[N];
+    for(int index = 0; index < M; index++)
+    {
+        scanf("%d", &arr1[index]);
+    }
+    for(int index = 0; index < N; index++)
+    {
+        scanf("%d", &arr2[index]);
+    }
+    int *mergedArr = mergeEven(M, arr1, N, arr2);
+    printf("Array 1: ");
+    for(int index = 0; index < M; index++)
+    {
+        printf("%d ", arr1[index]);
+    }
+    printf("\nArray 2: ");
+    for(int index = 0; index < N; index++)
+    {
+        printf("%d ", arr2[index]);
+    }
+    printf("\nMerged Array: ");
+    for(int index = 0; index < M+N; index++)
+    {
+        printf("%d ", mergedArr[index]);
+    }
+    return 0;
+}
