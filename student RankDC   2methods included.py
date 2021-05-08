7f2471c9e4019ef1d1d8eb452f51dad07ca1585d
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-05-08 14:17:16 +0530

    Update and rename student RankDC.py to student RankDC   2methods included.py

--- student RankDC   2methods included.py
+++ student RankDC   2methods included.py
@@ -2,3 +2,14 @@ n=int(input())   ###
  l=l(l(map(int,input().split())) ###
  x=int(input()) ###
 (lambda n,l,x:print(sorted(set(l))[::-1].index(l[x-1])+1))(int(input()),list(map(int,input().split())),int(input()))
+     
+     
+     
+     
+     
+     
+     2nd method:
+     
+n=int(input())
+L=[*map(int,input().split())]
+print((sorted(set(L),reverse=True).index(L[int(input())-1]))+1)
