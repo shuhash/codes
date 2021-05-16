24da2b604a09a0eed69f693a263f35ed44be9212
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-05-16 11:04:00 +0530

    Update Define class person DC.java

--- Define class person DC.java
+++ Define class person DC.java
@@ -1,17 +1,32 @@
-class pers{
-  private String name;
-  private int weight;
-  private double height;
-  pers(String named,int weight,double height){
-    name=named;
-    this.weight=weight;
-    this.height=height;
-  }
-  public double getBoadyMassIndex(){
-    return weight/(height*height);
-  }
-  @Override
-  public String toString(){
-    return "Name: "+name+"\nWeight: "+weight+"kg\nHeight :"+String.format("%.2f",height)+" m";
-  }
+import java.text.DecimalFormat;
+import java.util.*;
+class Person{
+    private String name;
+    private int weight;
+    private double height;
+    Person(String name,int weight,double height){
+        this.name=name;
+        this.height=height;
+        this.weight=weight;
+    }
+    public double getBodyMassIndex(){
+        return weight/(height*height);
+    }
+    @Override
+    public String toString(){
+        String precisedHeight=String.format("%.2f",height);
+        return "Name: "+name+"\n"+"Weight: "+weight+" kg"+"\n"+"Height: "+precisedHeight+" m";
+    }
+}
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine().trim();
+        int weight = sc.nextInt();
+        double height = sc.nextDouble();
+        Person person = new Person(name, weight, height);
+        System.out.println(person);
+        System.out.printf("BMI: %.1f", person.getBodyMassIndex());
+    }
 }
