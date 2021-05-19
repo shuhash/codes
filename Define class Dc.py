95e83c4c51a4b6203df9cec9ee439890a53ef98b
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-05-19 19:28:54 +0530

    Create Define class Dc.py

---
+++ Define class Dc.py
@@ -0,0 +1,8 @@
+class Test:
+    def __init__(self,name,time):
+        self.name=name
+        self.time=time
+    def getName(self):
+        return self.name
+    def getDuration(self):
+        return self.time
