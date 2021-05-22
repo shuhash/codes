84d273e21777200405e49b7047695011939e0f9e
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-05-22 19:15:23 +0530

    Create Print T even multiples of N.c

---
+++ Print T even multiples of N.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,i,c=o;
+    scanf("%d %d",&a,&b);
+    for(i=1;;i++)
+    {
+        if(i%2==0&&i%a==0)
+        {
+            printf("%d",i);
+            c++;
+            if(c==b)
+            break
+        }
+    }
+return 0;
+}
