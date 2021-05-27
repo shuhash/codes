1262323b71b10156c46c8e26da117d56e0ba1495
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-05-27 17:17:06 +0530

    Update Function GetColumnDC.c

--- Function GetColumnDC.c
+++ Function GetColumnDC.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct BoundedArray
+{
+    int SIZE;
+    int *arr;
+} boundedArray;
+
 boundedArray* getColumn(int R, int C, int matrix[][C],int K){
     boundedArray *ba=malloc(sizeof(boundedArray));
     ba->SIZE=R;
@@ -6,4 +15,25 @@ boundedArray* getColumn(int R, int C, int matrix[][C],int K){
         ba->arr[i]=matrix[i][k-1];
     }
     return ba;
+}
+int main()
+{
+    int R, C, K;
+    scanf("%d %d", &R, &C);
+    int matrix[R][C];
+    for(int row = 0; row < R; row++)
+    {
+        for(int col = 0; col < C; col++)
+        {
+            scanf("%d", &matrix[row][col]);
+        }
+    }
+    scanf("%d", &K);
+    boundedArray *bArr = getColumn(R, C, matrix, K);
+    printf("Column %d: ", K);
+    for(int index = 0; index < bArr->SIZE; index++)
+    {
+        printf("%d ", bArr->arr[index]);
+    }
+    return 0;
 }
