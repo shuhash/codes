dcd34e9632a267402bba0cc2da50e577e7718f69
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-05-29 13:55:59 +0530

    Create printAlaramTiming2DC.c

---
+++ printAlaramTiming2DC.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  char startTime[7],endTime[7];
+  int x;
+  scanf("%s\n%s\n%d",starttime,endTime,&X);
+  printAlaramtimings(startTime,endTime,X);
+  return 0;
+}
