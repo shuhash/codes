c7809b17a3a73f73cc6ab1fbfc8fd5a162229282
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-01 10:45:35 +0530

    Update Define class Student 2DC.java

--- Define class Student 2DC.java
+++ Define class Student 2DC.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Student{
     String name;
     int mark;
@@ -14,5 +15,21 @@ class Student{
     @Override
     public String toString(){
         return this.name+":"+this.mark;
+    }
+}
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        List<Student> students = new ArrayList<>();
+        for (int ctr = 1; ctr <= N; ctr++) {
+            String studentDetails[] = sc.nextLine().trim().split("\\s+");
+            students.add(new Student(studentDetails[0], Integer.parseInt(studentDetails[1])));
+        }
+        for (Student stud : students) {
+            stud.addExtraMarks(sc.nextInt());
+            System.out.println(stud);
+        }
     }
 }
