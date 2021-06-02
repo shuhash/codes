3f2c24f20f07e7374b73afc67ecba655b73540e2
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-02 10:55:29 +0530

    Update Function invert MatrixDC.c

--- Function invert MatrixDC.c
+++ Function invert MatrixDC.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 void InvertSubmatrix(int r,int c,int matrix[r][c],int srow,int scol,int k)
 {
   srow--;
@@ -12,4 +13,28 @@ void InvertSubmatrix(int r,int c,int matrix[r][c],int srow,int scol,int k)
           matrix[i][j]=!matrix[i][j];
         }
       }
+}
+int main()
+{
+    int R, C, K, sRow, sCol;
+    scanf("%d %d", &R, &C);
+    int matrix[R][C];
+    for(int row = 0; row < R; row++)
+    {
+        for(int col = 0; col < C; col++)
+        {
+            scanf("%d", &matrix[row][col]);
+        }
+    }
+    scanf("%d %d\n%d", &sRow, &sCol, &K);
+    invertSubmatrix(R, C, matrix, sRow, sCol, K);
+    for(int row = 0; row < R; row++)
+    {
+        for(int col = 0; col < C; col++)
+        {
+            printf("%d ", matrix[row][col]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
