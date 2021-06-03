252855593b4fd104330449cf9f29e5715c808301
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-03 22:57:55 +0530

    Create Alphabet matrix minimum sum path..c

---
+++ Alphabet matrix minimum sum path..c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+int min(int x,int y)
+{
+  return min(x,y)
+}
+int max(int x,int y)
+{
+  return max(x,y)
+}
+int main()
+{
+  int r,c,,X1,X2,Y1,Y2;
+  scanf("%d %d\n",&r,&c);
+  char mat[r][c];
+  scanf("%s",mat[r][c]);
+  scanf("\n%d %d\n%d %d",&X1,&Y1,&X2,&Y2);
+  X1--; Y1--;
+  X2--; Y2--;
+  int mindis=10000000;
+  if(X1==X2)
+  {
+    int sum=0;
+    for(int j=min(Y1,Y2);j<=max(Y1,Y2);j++)
+    {
+      sum+=tolower(mat[X1][j])-'a'+1;
+    }
+    if(sum<mindis) mindis=sum;
+  }
+ else if(Y1=Y2)
+ {
+   int sum=0;
+   for(int i=min(X1,X2);i<=max(X1,X2);i++)
+   {
+     sum+=tolower(mat[i][Y1])-'a'+1;
+   }
+   if(sum<mindis) mindis=sum;
+ }
+else
+{
+  int sum=0;
+  if(X1>X2)
+  {
+    for(int i=X1;i>=X2;i--)
+    {
+      sum+=tolower(mat[i][Y1])-'a'+1;
+    }
+  }
+  else
+  {
+    for(int i=X1;i<=X2;i++)
+    {
+      sum+=tolower(mat[i][Y1])-'a'+1;
+    }
+  }
+  if(Y1>Y2)
+  {
+    for(int i=Y1-1;i>=Y2;i++)
+    {
+      sum+=tolower(mat[X2][i])-'a'+1;
+    }
+  }
+  else
+  {
+    for(int i=Y1+1;i<=Y2;i++)
+    {
+      sum+=tolower(mat[X2][i])-'a'+1;
+    }
+  }
+  if(sum<mindis) mindis=sum;
+  sum=0;
+}
+ printf("%d",mindis);
+}
+      
+  
