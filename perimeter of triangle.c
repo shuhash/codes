d7b77fdedc56a5a85f38a9d11dc5e5f618584f4f
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-05 14:53:20 +0530

    Create perimeter of triangle.c

---
+++ perimeter of triangle.c
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+ int length,breadth;   
+ scanf("%d%d",&length,&breadth);   
+ int perimeter = 2*(length+breadth);   
+ printf("%d",perimeter);
+
+}
