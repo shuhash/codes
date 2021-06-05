2e16085637ee3b3852c2aeb798342f2dc5672b95
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-05 20:57:07 +0530

    Create  Repeat the input number.java

---
+++ Repeat the input number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+        System.out.println("You entered "+N);	
+
+	}
+}
