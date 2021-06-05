023f5bd5ffd877debd8fe8785cdb6cdad465ca80
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-05 14:55:57 +0530

    Create volume of cuboid.c

---
+++ volume of cuboid.c
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+int length,breadth,height;   
+scanf("%d%d%d",&length,&breadth,&height);  
+int volume = length*breadth*height;   
+printf("%d",volume);
+
+}
