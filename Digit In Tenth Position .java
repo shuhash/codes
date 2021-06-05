1c43ff26ea3d998f0dbcaf839c5abf98b362ec39
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-06 00:46:56 +0530

    Create  Digit In Tenth Position .java

---
+++ Digit In Tenth Position .java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int N = sc.nextInt();
+	    int tenthDigit = (N/10)%10;
+	    System.out.println(tenthDigit);
+
+	}
+}
