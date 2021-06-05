287799251cbb271ff589daa357a31dcd8a59c4a1
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-05 20:57:44 +0530

    Create Athlete & Medals Count.java

---
+++ Athlete & Medals Count.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    String name = sc.nextLine();
+	    int medalCount = sc.nextInt();
+	    System.out.println(name+" won "+medalCount+" medals");
+	}
+}
