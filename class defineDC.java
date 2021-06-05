44b9fe9d4e2d4565c9dd02abf0fe9f483016204d
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-05 11:51:10 +0530

    Update class defineDC.java

--- class defineDC.java
+++ class defineDC.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Event implements Comparable<Event>{
     private String title="";   
     private int registrationFee,duration; 
@@ -27,4 +28,23 @@ class Event implements Comparable<Event>{
         
     }
     
+}
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine().trim());
+        List<Event> events = new ArrayList<>();
+        for (int ctr = 1; ctr <= N; ctr++) {
+            String currEvent[] = sc.nextLine().trim().split("\\s+");
+            String title = currEvent[0];
+            int registrationFee = Integer.parseInt(currEvent[1]);
+            int duration = Integer.parseInt(currEvent[2]);
+            events.add(new Event(title, registrationFee, duration));
+        }
+        Collections.sort(events);
+        for (Event event : events) {
+            System.out.println(event);
+        }
+    }
 }
