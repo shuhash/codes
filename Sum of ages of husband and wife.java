b578c5531274a8029c408de52d9bf9407104aeb5
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-05 21:16:26 +0530

    Create  Sum of ages of husband and wife.java

---
+++ Sum of ages of husband and wife.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int husbandAge = sc.nextInt();
+		int wifeAge = sc.nextInt();
+		System.out.println(husbandAge+wifeAge);
+	}
+} S
