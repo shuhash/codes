73ac2bed7299c47dd51edaf789b8f6d49206c1a2
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-05 20:58:50 +0530

    Create  Space Separated String Input.java

---
+++ Space Separated String Input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String strval = sc.nextLine();
+        System.out.println(strval);
+    }
+}
