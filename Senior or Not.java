e8273b2bcefe9f6501dc3914e915e0834ac01703
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-05 21:56:01 +0530

    Create  Senior or Not.java

---
+++ Senior or Not.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int age= sc.nextInt();
+	    if(age >= 60){
+	        System.out.println("senior");
+	    }else{
+	        System.out.println("notsenior");
+	    }
+
+	}
+}
