d6148410310f0cd26af627b1ce9c46fb26833411
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-14 22:01:07 +0530

    Create  Deletion at the first.c

---
+++ Deletion at the first.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<malloc.h>
+typedef struct node
+{
+ int data;
+ struct node *prev , *next;
+}NODE;
+void insertAtEnd(NODE**, NODE**, int);
+void displayForward(NODE*);
+void deleteAtFirst(NODE** start , NODE**end)
+{
+ if(*start == *end)
+ {
+ *start = *end = 0;
+ }
+ else
+ {
+
+ (*start)->next -> prev=0;
+ (*start) = (*start)->next;
+}
+}
+int main()
+{
+NODE *start , *end ;
+start = end = 0;
+int N , val , itr ;
+scanf("%d" , &N);
+for(itr = 1 ; itr <= N ; itr++)
+{
+ scanf("%d" , &val);
+ insertAtEnd(&start , &end , val);
+}
+deleteAtFirst(&start , &end);
+displayForward(start);
+ return 0;
+}
+void displayForward(NODE *start)
+{
+ NODE *tptr;
+ for(tptr = start ; tptr ; tptr = tptr -> next)
+ printf("%d " , tptr -> data);
+}
+void insertAtEnd(NODE** start , NODE** end , int val)
+{
+ NODE *newnode;
+ newnode = (NODE*)calloc(1 , sizeof(NODE));
+ newnode -> data = val;
+ if(*start == 0)
+ *start = *end = newnode;
+ else
+ {
+ newnode -> prev = *end ;
+ (*end)->next = newnode;
+ *end = newnode;
+ }
+}
