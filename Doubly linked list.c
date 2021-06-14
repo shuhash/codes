d7cb0f831fb9308ee52618bd2d351d497093d6bb
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-14 21:59:49 +0530

    Create Doubly linked list.c

---
+++ Doubly linked list.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<malloc.h>
+typedef struct node
+{
+ int data;
+ struct node *prev , *next;
+}NODE;
+void insertAtEnd(NODE**, NODE**, int);
+void displayForward(NODE*);
+void insertAtEnd(NODE** start , NODE** end , int val)
+{
+ NODE *newnode;
+ newnode = (NODE*)calloc(1 , sizeof(NODE));
+ newnode -> data = val;
+ if(*start == 0)
+ *start = *end = newnode;
+ else
+ {
+
+ newnode -> prev = *end ;
+ (*end)->next = newnode;
+ *end = newnode;
+ }
+}
+int main()
+{
+NODE *start , *end ;
+start = end = 0;
+int N , val , itr ;
+scanf("%d" , &N);
+for(itr = 1 ; itr <= N ; itr++)
+{
+ scanf("%d" , &val);
+ insertAtEnd(&start , &end , val);
+}
+displayForward(start);
+ return 0;
+}
+void displayForward(NODE *start)
+{
+ NODE *tptr;
+ for(tptr = start ; tptr ; tptr = tptr -> next)
+ printf("%d " , tptr -> data);
+}
