a849411a2c3b60d4aec6dc90c2a6860a597b00f8
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-15 15:33:05 +0530

    Create Merge columns fold to the right.py

---
+++ Merge columns fold to the right.py
@@ -0,0 +1,14 @@
+r,c = map(int,input().split())
+mat = [list(map(int,input().split()))for i in '.'*r]
+n = int(input())
+for row in range(r):    
+    for col in range(c):        
+        if((n-1-col)>=0 and (n+col)<c):
+            print(mat[row][n-1-col]+mat[row][n+col],end=" ")
+        elif(n-1-col>=0):            
+            print(mat[row][n-1-col],end=" ")        
+        elif(n+col<r):            
+            print(mat[row][n+col],end=" ")        
+        else:            
+            break    
+    print()
