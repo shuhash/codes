f453bb6f3307a65a447ef5c56dc0f53ff91c8031
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-18 10:01:44 +0530

    Create todayDC.py

---
+++ todayDC.py
@@ -0,0 +1,9 @@
+a=input().split()
+temp=1
+for i in range(len(a)):
+    for j in range(len(a)):
+        if (i!=j and a[i]+a[j]==a[i]+a[j]):
+            print((a[i]+a[j])[::-1])
+            temp=0
+if temp==0:
+    print(-1)
