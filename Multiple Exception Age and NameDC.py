ac31335bca031522f54833b3aac4552a12710bc6
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-19 13:00:44 +0530

    Create Multiple Exception Age and NameDC.py

---
+++ Multiple Exception Age and NameDC.py
@@ -0,0 +1,20 @@
+class MaximumLengthExceededException(Exception):
+    def __init__(self,a):
+        self.a=a
+    def __str__(self):
+        return self.a
+class NegativeValueException(Exception):
+    def __init__(self,a):
+        self.a=a
+    def __str__(self):
+        return self.a
+name = input().strip()
+age = int(input().strip())
+try: 
+  if len(name) > 20: 
+    raise MaximumLengthExceededException("name cannot contain more than 20 characters") 
+  if age <= 0: 
+    raise NegativeValueException("age cannot be negative or zero") 
+  print(name, age)
+except (MaximumLengthExceededException, NegativeValueException) as e: 
+  print(e)        
