922a506534ab98959a6fcc9c67a0211fbeb7468c
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-06-28 22:06:14 +0530

    Create  Maximum number of balls triangleDC.java

---
+++ Maximum number of balls triangleDC.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+public class Hello
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    int n=s.nextInt();
+	    String str="";
+	    for(int i=1;i<=n;i++)
+	    {
+	        str=str+s.next();
+	        System.out.println(str);
+	        str=str.substring(0,i);
+	    }
+	    
+	}
+}
