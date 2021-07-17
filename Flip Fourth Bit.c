346069b9fdcea4e2a095d1349f9b9441b2adde66
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-07-17 20:09:43 +0530

    Update Flip Fourth Bit.c

--- Flip Fourth Bit.c
+++ Flip Fourth Bit.c
@@ -1,5 +1,38 @@
+Flip Fourth Bit
 
+The program must accept an integer N as the input. The program must flip (invert) the 4th bit from the end of the binary representation of N. Then the program must print the revised value of N as the output. Please fill in the blanks with code so that the program runs successfully.
+Boundary Condition(s):
+Input Format:
 
+The first line contains N.
+
+Output Format:
+
+The first line contains the revised value of N after inverting the 4th bit from the end of the binary representation of N.
+
+Example Input/Output 1:
+
+Input: 25
+
+Output:
+
+17
+
+Explanation:
+
+The binary representation of 25 is 11001.
+
+After flipping the 4th bit from the end, the binary representation becomes 10001. The decimal equivalent of 10001 is 17, which is printed as output
+
+Example Input/Output 2:
+
+Input 15
+
+Output:
+
+7
+
+Max Execution Time Limit: 50 millisecs
 
 
 
