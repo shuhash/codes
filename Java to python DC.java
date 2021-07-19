2f1703bc72423a08b0acdfe467c60f78de9a7b63
Shuhash J S <60608897+shuhash@users.noreply.github.com>
2021-07-19 20:16:58 +0530

    Create Java to python DC.java

---
+++ Java to python DC.java
@@ -0,0 +1,21 @@
+Please convert the following Java code to Python so that the Python program secutes successfuly passing the test cases
+
+import java.util.*;
+
+public class Hello {
+
+public static void main(String[] args) {
+
+Scanner senex Scanner(System.in);
+
+StringBuilder sb= new Stringbuilder(); 
+while (sc.hasliextLine()) {
+       String cuertine[] sc.nextLine().trim().split("\\");
+       for (String word currLine) {
+         sb.append(word.charAt(word. length()-1)); 
+}
+}
+
+System.out.println(sb.reverse().toString());
+}
+}
